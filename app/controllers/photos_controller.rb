c95e87fa045edf74c75df049f39383ddf97825e9
Nikitamishra2511 <nmishra1@chicagobooth.edu>
2025-05-23 08:30:59 +0000

    jkks

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,44 +1,72 @@
 class PhotosController < ApplicationController
-     skip_before_action(:authenticate_user!, { :only => [:index, :show] })
+  skip_before_action(:authenticate_user!, { :only => [:index, :show] })
 
-     def index
-       all_photos       = Photo.all
-       public_photos    = []
+  def index
+    all_photos    = Photo.all
+    public_photos = []
 
-       all_photos.each do |p|
-         owner = User.where({ :id => p.owner_id }).at(0)
-         next if owner.private
+    all_photos.each do |p|
+      owner = User.where({ :id => p.owner_id }).at(0)
+      next if owner.private
 
-         public_photos.push(p)
-       end
+      public_photos.push(p)
+    end
 
-       @list_of_photos = public_photos
-       render({ :template => "photos/index" })
-     end
+    @list_of_photos = public_photos
+    render({ :template => "photos/index" })
+  end
 
-     def new
-       render({ :template => "photos/new" })
-     end
+  def new
+    render({ :template => "photos/new" })
+  end
 
-     def create
-       the_photo                 = Photo.new
-       the_photo.owner_id        = session.fetch("user_id")
-       the_photo.caption         = params.fetch("caption")
-       the_photo.image           = params.fetch("image")
-       the_photo.comments_count  = 0
-       the_photo.likes_count     = 0
-       the_photo.save
+  def create
+    the_photo                  = Photo.new
+    the_photo.owner_id         = session.fetch("user_id")
+    the_photo.caption          = params.fetch("caption")
+    the_photo.image            = params.fetch("image")
+    the_photo.comments_count   = 0
+    the_photo.likes_count      = 0
+    the_photo.save
 
-       return redirect_to("/photos/#{ the_photo.id }",
-                          { :notice => "Photo added successfully." })
-     end
+    return redirect_to("/photos/#{ the_photo.id }",
+                       { :notice => "Photo added successfully." })
+  end
 
-     def show
-       the_id        = params.fetch("an_id")
-       matching_photos = Photo.where({ :id => the_id })
-       @the_photo    = matching_photos.at(0)
-       render({ :template => "photos/show" })
-     end
+  def show
+    the_id         = params.fetch("an_id")
+    matching_photos = Photo.where({ :id => the_id })
+    @the_photo     = matching_photos.at(0)
+    render({ :template => "photos/show" })
+  end
 
-     # …edit, update, destroy, feed, discovery…
-   end
+  def edit
+    the_id         = params.fetch("an_id")
+    matching_photos = Photo.where({ :id => the_id })
+    @the_photo     = matching_photos.at(0)
+    render({ :template => "photos/edit" })
+  end
+
+  def update
+    the_id         = params.fetch("an_id")
+    matching_photos = Photo.where({ :id => the_id })
+    the_photo      = matching_photos.at(0)
+    the_photo.caption = params.fetch("caption")
+    the_photo.image   = params.fetch("image")
+    the_photo.save
+
+    return redirect_to("/photos/#{ the_photo.id }",
+                       { :notice => "Photo updated successfully." })
+  end
+
+  def destroy
+    the_id         = params.fetch("an_id")
+    matching_photos = Photo.where({ :id => the_id })
+    the_photo      = matching_photos.at(0)
+    the_photo.destroy
+
+    return redirect_to("/photos", { :notice => "Photo deleted successfully." })
+  end
+
+  # feed & discovery live in UsersController
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,57 @@
 class UsersController < ApplicationController
-   # allow non-signed-in visitors to see index only; show remains behind auth
-   skip_before_action(:authenticate_user!, { :only => [:index] })
+  # anyone can see the list of users
+  skip_before_action(:authenticate_user!, { :only => [:index] })
 
-   def index
-     @list_of_users = User.all
-     render({ :template => "users/index" })
-   end
+  def index
+    @list_of_users = User.all
+    render({ :template => "users/index" })
+  end
 
-   def show
-     the_username   = params.fetch("an_id")
-     matching_users = User.where({ :username => the_username })
-     @the_user      = matching_users.at(0)
+  def show
+    the_username    = params.fetch("username")
+    matching_users  = User.where({ :username => the_username })
+    @the_user       = matching_users.at(0)
+    render({ :template => "users/show" })
+  end
 
-     render({ :template => "users/show" })
-   end
- end
+  def feed
+    current_user = User.where({ :id => session.fetch("user_id") }).at(0)
+
+    # everyone I’m following (accepted)
+    accepted_requests = FollowRequest.where({ :follower_id => current_user.id,
+                                              :status      => "accepted" })
+    followee_ids      = accepted_requests.map { |req| req.followee_id }
+
+    @list_of_photos = Photo.where({ :owner_id => followee_ids })
+    render({ :template => "users/feed" })
+  end
+
+  def liked_photos
+    current_user = User.where({ :id => session.fetch("user_id") }).at(0)
+
+    # photos I’ve liked
+    my_likes    = Like.where({ :fan_id => current_user.id })
+    photo_ids   = my_likes.map { |like| like.photo_id }
+
+    @liked_photos = Photo.where({ :id => photo_ids })
+    render({ :template => "users/liked_photos" })
+  end
+
+  def discover
+    current_user = User.where({ :id => session.fetch("user_id") }).at(0)
+
+    # photos liked by people I follow
+    accepted_requests = FollowRequest.where({ :follower_id => current_user.id,
+                                              :status      => "accepted" })
+    followee_ids      = accepted_requests.map { |req| req.followee_id }
+
+    their_likes   = Like.where({ :fan_id => followee_ids })
+    liked_ids     = their_likes.map { |like| like.photo_id }.uniq
+
+    @discovered_photos = Photo.where({ :id => liked_ids })
+    render({ :template => "users/discover" })
+  end
+
+  # You’ll rely on FollowRequestsController#create/destroy for actual follow/unfollow.
+  # The UsersController only displays the right buttons/links.
+end
