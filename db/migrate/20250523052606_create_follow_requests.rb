e6b1118ddd80d894eddba1a396f0f7c4becdf507
Nikitamishra2511 <nmishra1@chicagobooth.edu>
2025-05-29 20:28:34 +0000

    mc

--- db/migrate/20250523052606_create_follow_requests.rb
+++ db/migrate/20250523052606_create_follow_requests.rb
@@ -1,7 +1,7 @@
 class CreateFollowRequests < ActiveRecord::Migration[7.1]
   def change
     create_table :follow_requests do |t|
-      t.integer :sender_id
+      t.integer :fan_id
       t.integer :recipient_id
       t.string :status
 
